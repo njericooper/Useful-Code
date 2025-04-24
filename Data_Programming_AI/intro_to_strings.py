b6851c915c9c511a92a234748a31b9b073f1fead
njericooper <njericooper@gmail.com>
2025-04-24 08:04:51 -0400

    slicing strings

--- Data_Programming_AI/intro_to_strings.py
+++ Data_Programming_AI/intro_to_strings.py
@@ -8,3 +8,12 @@ print(favorite_fruit[1])
 my_name = "Njeri"
 
 first_initial = my_name[0]
+
+#Slice of string
+
+first_name = "Rodrigo"
+last_name = "Villanueva"
+
+new_account = (last_name[0:5])
+
+temp_password = (last_name[2:6])
